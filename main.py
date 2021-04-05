614bd9b03175a9b5e0e0411fe5156b36bc0016d4
Albert Gawin <166648@stud.prz.edu.pl>
2021-04-05 15:05:20 +0200

    test

--- main.py
+++ main.py
@@ -1,18 +1,4 @@
-import pandas as pd
+from collections import Counter
+grades = [83,95,87,95,70,0,85,82,100,67,73,77,0]
 
-kolumny = \
-    ["N","M","D","H","DBT","RH","HR","WS","WD","ITH",
-     "IDH","ISH","TSKY","N_0","N_30","NE_30","E_30","SE_30","S_30","SW_30",
-     "W_30","NW_30","N_45","NE_45","E_45","SE_45","S_45","SW_45","W_45","NW_45",
-     "N_60","NE_60","E_60","SE_60","S_60","SW_60","W_60","NW_60","N_90","NE_90",
-     "E_90","SE_90","S_90","SW_90","W_90","NW_90"]
-
-dane_bialystok = pd.read_fwf("Data/bialystok.txt")
-dane_bydgoszcz = pd.read_fwf("Data/bydgoszcz.txt")
-dane_chojnie = pd.read_fwf("Data/chojnice.txt")
-dane_elblag = pd.read_fwf("Data/elblag.txt")
-
-dane_bialystok.columns = kolumny
-dane_bydgoszcz.columns = kolumny
-dane_chojnie.columns = kolumny
-dane_elblag.columns = kolumny
+histogram = Counter(min(grade // 10 * 10, 90) for grade in grades)
